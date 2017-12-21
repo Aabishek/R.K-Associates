81e8e9a5be0605c83bb000359133d6cb2455e9ac
Aabishek <34753397+Aabishek@users.noreply.github.com>
2017-12-22 02:31:58 +0530

    Add files via upload

---
+++ indian technocrat/Footer.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sign Up</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	</title>
+	<style type="text/css">
+  		div.h a{
+  			color:white;
+  			text-decoration: none;
+  		}
+  	</style>
+</head>
+<body>
+	<div class="container-fluid" style="background-color: rgb(79,79,79); margin-top: 100px;">
+			<div class="row" style=" margin-top: 40px; color: white;">
+				<div class="h">
+					<div class="col-sm-3">
+						<img src="Image/ff.gif" style="width: 300px; margin-top: -42px;">
+					
+					</div>
+				<div class="col-sm-3">
+					<center><a href="#"><h5 style="color: white;"><b> Menu</b></h5></a></center>
+					<a href="#"><center><h6>Our Services</h6></a></center>
+					<center><a href="#"><h6>Field Of Experts</h6></a></center>
+					<center><a href="#"><h6>Experience</h6></a></center>
+					<center><a href="#"><h6>Key Qualification</h6></a></center>
+					<center><a href="#"><h6>Clients</h6></a></center>
+					<center><a href="#"><h6>Career</h6></a></center>
+					<center><a href="#"><h6>Contact</h6></a></center>
+				</div>
+				<div class="col-sm-3">
+					<center><a href="#"><h5 style="color: white;"><b>Support</b></h5></a></center>
+					<center>
+						<a href="#">
+							<span style="font-size: 30px;">
+								<span><i class="fa fa-facebook-official" aria-hidden="true"></i></span>
+								<span><i class="fa fa-twitter" aria-hidden="true"></i></span>
+								<span><i class="fa fa-linkedin-square" aria-hidden="true"></i></span>
+								<span><i class="fa fa-google-plus" aria-hidden="true"></i></span>
+							</span>
+						</a>
+					</center>
+					<center>
+						<a href="#">
+							<span  style="font-size: 30px;">
+								
+							</span>
+						</a>
+					</center>
+					
+				</div>
+				<div class="col-sm-3">
+					<center><a href="#"><h5 style="color: white;"><b>Support</b></h5></a></center>
+					<center><a href="#"><h6>Feedback & Enquiry&nbsp;&nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;FAQs</h6></a></center>
+					<hr>
+					<center><a href="#"><h6>We are here to help 24/7<br>07011117111</h6></a></center>
+					<hr>
+					<center><a href="#"><h6><b>Support E-mail Address:</b><br>help@myfaithhouse.com</h6></a></center>	
+				</div>
+				</div>
+			</div>
+	</div>
+</body>
+</html>
---
+++ indian technocrat/Index.php
@@ -0,0 +1,298 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<title> R.K Association </title>
+	<style>
+      #map {
+        height: 400px;
+        width: 100%;
+       }
+    </style>
+  </head>
+<body>
+<div class="container-fluid" style="width: 100%; background-color: #222;">	
+<nav class="navbar navbar  navbar-inverse navbar-fixed-top" style="border:none; opacity: 0.8">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle navbar-default" data-toggle="collapse" data-target="#myNavbar">
+        <span><b>Menu</b></span>                       
+      </button>
+      <a class="navbar-brand" href="#">R.K Association</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right ">
+        <li class="active dropdown">
+        	<a class="dropdown-toggle" data-toggle="dropdown" href="#" >Home <span class="caret"></span></a>
+        	<ul class="dropdown-menu">
+            <li><a href="#">Home </a></li>
+            <li class="divider"></li>
+            <li><a href="#">About Us </a></li>
+            <li class="divider"></li>
+            <li><a href="#">Top Management</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Values</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Vision & Strength</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Quality Policy</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Our Services <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Field Study</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Design Services</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Construction Support Services</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Field Of Experts <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Highway And Roads</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Bridges & Structure</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Railways</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Airport</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#"> Experience <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">DPR</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Construcion & Supervision</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Detailed Engenering</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Proof Checking</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Pre-Bid Advisiory</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Road Safety Studies</a></li>
+            <li class="divider"></li>
+            <li><a href="#">PMC Services</a></li>
+          </ul>
+        </li>
+        <li><a href="#">Key Qualification</a></li>
+        <li><a href="#">Clients</a></li>
+        <li><a href="#">Career</a></li>
+        <li><a href="#">Contact</a></li>
+        
+
+
+      </ul>
+    </div>
+  </div>
+</nav>
+</div>
+
+
+
+
+
+						<!--...........................Crousel Section..................................-->
+
+
+
+<div class="container-fluid" style="margin-top: 10%;">
+	<div class="container" style="margin-bottom: 20px;">
+		<center><h1><b style="color: #57527fd1;"> Our Infrastructure </b></h1></center>
+	</div>
+	
+	
+  <div id="myCarousel" class="carousel slide" data-ride="carousel" >
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+      <li data-target="#myCarousel" data-slide-to="3"></li>
+      <li data-target="#myCarousel" data-slide-to="4"></li>
+      <li data-target="#myCarousel" data-slide-to="5"></li>
+      <li data-target="#myCarousel" data-slide-to="6"></li>
+      <li data-target="#myCarousel" data-slide-to="7"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" >
+
+      <div class="item active">
+        <img src="Image/1.jpg" alt="Los Angeles" style="width:100%;">
+        <div class="carousel-caption">
+          <h3> Road Infrastructure </h3>
+          <p>As Per As Our Services</p>
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="Image/2.jpg" alt="Chicago" style="width:100%;">
+        <div class="carousel-caption">
+          <h3> Road Infrastructure </h3>
+          <p>As Per As Our Services</p>
+        </div>
+      </div>
+
+       <div class="item">
+        <img src="Image/3.jpg" alt="Chicago" style="width:100%;">
+        <div class="carousel-caption">
+          <h3> Road Infrastructure </h3>
+          <p>As Per As Our Services</p>
+        </div>
+      </div>
+
+       <div class="item">
+        <img src="Image/4.jpg" alt="Chicago" style="width:100%;">
+        <div class="carousel-caption">
+          <h3> Road Infrastructure </h3>
+          <p>As Per As Our Services</p>
+        </div>
+      </div>
+
+       <div class="item">
+        <img src="Image/5.jpg" alt="Chicago" style="width:100%;">
+        <div class="carousel-caption">
+          <h3> Road Infrastructure </h3>
+          <p>As Per As Our Services</p>
+        </div>
+      </div>
+    
+      <div class="item">
+        <img src="Image/6.jpg" alt="New York" style="width:100%;">
+        <div class="carousel-caption">
+          <h3> Road Infrastructure </h3>
+          <p>As Per As Our Services</p>
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="Image/7.jpg" alt="New York" style="width:100%;">
+        <div class="carousel-caption">
+          <h3> Road Infrastructure </h3>
+          <p>As Per As Our Services</p>
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="Image/8.jpg" alt="New York" style="width:100%;">
+        <div class="carousel-caption">
+          <h3> Road Infrastructure </h3>
+          <p>As Per As Our Services</p>
+        </div>
+      </div>
+  
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+	
+</div>
+
+
+
+
+
+<!--............................   Rk Assocition Project..................................-->
+
+
+
+<div class="container" style="margin-top: 30px;  width: 98%">
+<div class="row">
+
+	
+
+	<div class="col-sm-3"  style="border:0.5px solid lightgrey; border-radius: 10px; background-color: lightgrey;">
+		<center><h3><b>R.K Association</b></h3></center>
+		<marquee id="test" behavior="scroll" direction="up" height="200" scrolldelay="100" scrollamount="4" onMouseOver="document.all.test.stop()" onMouseOut="document.all.test.start()">
+ 		<ul style="list-style: none; height: 206px;">
+			<li><a href="#"><b><h5>What's New?????</b></h5></a><li>
+			------------------------------------------
+			<li><a href="#"><b><h5>Under Construction Project</b></h5></a><li>
+			------------------------------------------
+			<li><a href="#"><b><h5>Completed projects</b></h5></a><li>
+			------------------------------------------
+			<li><a href="#"><b><h5>Ongoing Projects</b></h5></a><li>
+ 		</ul>
+                </marquee>
+	</div>
+
+	<div class="col-sm-3" style=" border-radius: 10px;">
+		<img src="Image/11.gif" style="width: 100%; height: 262px;">
+	</div>
+
+
+	<div class="col-sm-3" style=" lightgrey; border-radius: 10px;">
+		<video width="100%" height="262" controls>
+  			<source src="Image/infrestructure.mp4" type="video/mp4">
+  		</video>
+	</div>
+
+
+	<div class="col-sm-3" style=" border-radius: 10px;">
+    <div id="map"  style="width: 100%; height: 262px;"></div>
+    <script>
+      function initMap() {
+        var uluru = {lat: 30.353663, lng: 76.417184};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyClQ4oaZTy8bEMKg-IzVbYPUIsIaSMrRzM&callback=initMap">
+    </script>
+</div>
+</div>
+</div>
+
+<div class="container-fluid" style="margin-top: 30px;">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-2">
+				<button class="btn btn-info">
+					Services Offered
+				</button>
+				
+			</div>
+			<div class="col-sm-10">
+				<marquee behavior="scroll" direction="left" onmouseover="this.stop();" onmouseout="this.start();">
+					<b><a href="#">Feasibility Study</a>   * Preparation of Detailed Report * Pre-bid Services  * Road Safety Consultancy  * Proof Check Consultancy  * Construction Supervision Consultancy | Detailed Design Consultancy * Geotechnical Studies * Topographical Survey * Hydrological Survey * Traffic Survey  * Economic and Financial Analysis * Transaction Advisory * Project Development Services</b>
+				</marquee>
+			</div>
+		</div>
+	</div>
+</div>
+<?php
+	include 'footer.php';
+
+?>
+
+
+
+</body>
+</html>
